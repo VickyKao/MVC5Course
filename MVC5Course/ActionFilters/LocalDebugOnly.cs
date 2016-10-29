f792fd74aabe79011d9659c601d8beccbb7b5cc1
Vicky Kao <vickykao53@hotmail.com>
2016-10-29 17:06:42 +0800

    45 練習自訂 Action Filter 動作過濾器

---
+++ MVC5Course/ActionFilters/LocalDebugOnly.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers {
+    class LocalDebugOnlyAttribute : ActionFilterAttribute {
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext) {
+            //如果是從遠端登入就轉址
+            if (!filterContext.HttpContext.Request.IsLocal) {
+                filterContext.Result = new RedirectResult("/");
+            }
+
+
+            //base.OnActionExecuting(filterContext);
+        }
+
+    }
+}
---
+++ MVC5Course/ActionFilters/ShareData.cs
@@ -0,0 +1,27 @@
+﻿using MVC5Course.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers {
+    public class Share頁面上常用的ViewBag變數資料 : ActionFilterAttribute {
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext) {
+            filterContext.Controller.ViewData["Temp1"] = "暫存資料 Temp1";
+
+            var b = new ClientLoginViewModel() {
+                FirstName = "Vicky",
+                LastName = "Kao"
+            };
+
+            filterContext.Controller.ViewData["Temp2"] = b;
+            filterContext.Controller.ViewBag.Temp3 = b;
+
+            //base.OnActionExecuting(filterContext);
+        }
+
+
+    }
+}

5a9e3eaab07c482dc948c4b1c3649d3f39ab2100
Vicky Kao <vickykao53@hotmail.com>
2016-10-30 09:14:17 +0800

    46 開啟 HandleError 的預設錯誤頁面，建立會發生例外的 Action 與調整 Web.config 開啟自訂錯誤模式

--- MVC5Course/ActionFilters/ShareData.cs
+++ MVC5Course/ActionFilters/ShareData.cs
@@ -9,6 +9,8 @@ namespace MVC5Course.Controllers {
     public class Share頁面上常用的ViewBag變數資料 : ActionFilterAttribute {
 
         public override void OnActionExecuting(ActionExecutingContext filterContext) {
+            //開始時間
+            filterContext.Controller.ViewData["StrTime"] = DateTime.Now;
             filterContext.Controller.ViewData["Temp1"] = "暫存資料 Temp1";
 
             var b = new ClientLoginViewModel() {
@@ -22,6 +24,12 @@ namespace MVC5Course.Controllers {
             //base.OnActionExecuting(filterContext);
         }
 
+        public override void OnActionExecuted(ActionExecutedContext filterContext) {
+            //結束時間
+            filterContext.Controller.ViewData["EndTime"] = DateTime.Now;
+
+            //base.OnActionExecuted(filterContext);
+        }
 
     }
 }
